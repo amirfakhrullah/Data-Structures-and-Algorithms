b45f46ea9d8e30f843e55a52f932c1c7620fd56a
amirfakhrullah <amirfakhrullah96@gmail.com>
2021-12-23 00:10:38 +0800

    queues and stacks

---
+++ stacks/js/Stack.js
@@ -0,0 +1,48 @@
+const LinkedList = require('./LinkedList');
+
+class Stack {
+    constructor(maxSize = Infinity) {
+        this.stack = new LinkedList();
+        this.maxSize = maxSize;
+        this.size = 0;
+    }
+
+    hasRoom() {
+        return this.maxSize > this.size;
+    }
+
+    isEmpty() {
+        return this.size === 0;
+    }
+
+    push(value) {
+        if (this.hasRoom()) {
+            this.stack.addToHead(value);
+            this.size++;
+        } else {
+            throw new Error('Stack is full');
+        }
+    }
+
+    pop() {
+        if (!this.isEmpty()) {
+            const value = this.stack.removeHead();
+            this.size--;
+            return value;
+        }
+        else {
+            console.log('Stack is empty!');
+        }
+    }
+
+    peek() {
+        if (!this.isEmpty()) {
+            return this.stack.head.data;
+        } else {
+            return null;
+        }
+    }
+
+}
+
+module.exports = Stack;
